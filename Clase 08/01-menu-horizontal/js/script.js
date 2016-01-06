b474833008a238abbf868b1ecd7f4bc018808d95
Sergio <sergiohidalgocaceres@gmail.com>
2016-01-05 21:38:57 -0500

    Clase 08

---
+++ Clase 08/01-menu-horizontal/js/script.js
@@ -0,0 +1,10 @@
+$(".menu").on("click", function(){
+	/*Esto se va a ejecutar cuando alguien
+		haga clic sobre el icono del menu
+
+		addClass para añadir una clase
+		removeClass para remover una clase
+		toggleClass para alternar una clase
+	*/
+	$(".zonaMenu").toggleClass("mostrarMenu");	
+})
